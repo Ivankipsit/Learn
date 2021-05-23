ce456ae1373e5a4692cfc8257528d0256138b46e
Ivankipsit <ivankipsit@gmail.com>
2021-05-23 13:32:23 +0530

    nicee

---
+++ google/String.py
@@ -0,0 +1,6 @@
+import string
+#inputstr = str(input())
+lista = list(string.ascii_lowercase)
+listz = list(string.ascii_lowercase[::-1])
+listA = list(string.ascii_uppercase)
+splchar = [chr(i) for i in range(33,65)]
